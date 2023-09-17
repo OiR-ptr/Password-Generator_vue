eca774bbbe0f208fd98273ce1a87df32cbff4a83
DESKTOP-UQMVDN2\myr_r <myr_ryusuke@hotmail.co.jp>
2023-09-17 11:59:03 +0000

    :wrench: lintツールを追加

---
+++ .eslintrc.cjs
@@ -0,0 +1,22 @@
+/* eslint-env node */
+require("@rushstack/eslint-patch/modern-module-resolution");
+
+module.exports = {
+  root: true,
+  extends: [
+    "plugin:vue/vue3-recommended",
+    "eslint:recommended",
+    "@vue/eslint-config-typescript",
+  ],
+  parser: "vue-eslint-parser",
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  env: {
+    browser: true,
+    node: true,
+    webextensions: true,
+  },
+};
